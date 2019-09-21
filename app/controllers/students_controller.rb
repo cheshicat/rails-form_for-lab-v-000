4c4a4abb52120a68e5a71f2143cc11d33c176c9a
Allison Howard <alliemhow@gmail.com>
2019-09-21 23:15:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,16 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+   redirect_to student_path(@student)
+ end
+
   private
 
   def student_params
