8751c611a03396705db331e03c16a09c5c248e40
Allison Howard <alliemhow@gmail.com>
2019-09-21 22:45:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,18 @@
 class StudentsController < ApplicationController
+  def new
+    @students = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit!
+  end
+
 end
