a84207d2320ac0445de12e483fdb6ca7e26cf99b
Allison Howard <alliemhow@gmail.com>
2019-09-21 22:46:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :school_classes, except: [:destroy, :index]
   resources :students, except: [:destroy, :index]
-
 end
